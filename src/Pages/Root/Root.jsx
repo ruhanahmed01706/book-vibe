5ec2131dca050189bda460033c934dc0ef874695
ruhan <mdruhanahmed49@gmail.com>
2025-10-31 02:05:22 +0600

    navber & footer add

---
+++ src/Pages/Root/Root.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navber from '../../Components/Header/Navber';
+import { Outlet } from 'react-router';
+import Footer from '../../Components/Header/Footer/Footer';
+
+const Root = () => {
+    return (
+        <div className='max-w-6xl mx-auto'>
+            <Navber></Navber>
+            <Outlet></Outlet>
+            <Footer></Footer>
+            
+        </div>
+    );
+};
+
+export default Root;
